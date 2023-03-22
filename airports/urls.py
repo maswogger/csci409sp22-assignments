0979fd8b212e621d117addf045ef36c8a6897d29
maswogger <maswogger@coastal.edu>
2023-03-21 21:18:27 -0400

    week 9 assignment

--- airports/urls.py
+++ airports/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('/', views.index),
 
     path('/<str:airport_code>', views.airport_info),
 ]
--- airports/views.py
+++ airports/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Airport
+
 # Create your views here.
+#
 def index(request):
     airports = Airport.objects.all()
-    airport_list = ', '.join([a.airport_code for a in airports])
-    return HttpResponse('Showing all airports: ' + airport_list);
+    context = {'airports': airports}
+    return render(request, 'airports/index.html', context)
+
 def airport_info(request, airport_code):
     airport = Airport.objects.get(airport_code=airport_code)
-    return HttpResponse('showing info for airport: ' + airport.name + "- " + airport.airport_code)
+    context = {'airport': airport}
+    return render(request, 'airports/airports.html', context)
